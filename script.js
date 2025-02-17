ec6fd22efcb671f643b77b96a5626bd982c47974
Sreyleak Deth <sreyleak.deth19@gmail.com>
2025-02-17 23:08:14 +0700

    Close #3 Enhancement the event section and remove contact form

--- script.js
+++ script.js
@@ -7,3 +7,26 @@
       }, 1000);
     };
   })();
+
+
+  // change sai_logo base on darkmode
+  const themeToggle = document.getElementById("theme-mode");
+  const logo = document.getElementById("logo");
+
+  if (localStorage.getItem("theme") === "dark") {
+    themeToggle.checked = true;
+    logo.src = "assets/img/svg/sai_logo_white.svg";
+    document.body.classList.add("dark-theme");
+  }
+
+  themeToggle.addEventListener("change", function () {
+    if (themeToggle.checked) {
+      logo.src = "assets/img/svg/sai_logo_white.svg"; 
+      document.body.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark");
+    } else {
+      logo.src = "assets/img/svg/sai_logo_black.svg"; 
+      document.body.classList.remove("dark-theme");
+      localStorage.setItem("theme", "light");
+    }
+  });
